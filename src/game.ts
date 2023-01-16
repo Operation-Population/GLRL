f283ec28d07f24115e9ff29d06b16a91fddfb40a
Giorgio <65674128+Gaffomdz@users.noreply.github.com>
2023-01-16 17:06:57 +0300

    first commit

---
+++ src/game.ts
@@ -0,0 +1,13 @@
+import { Dash_TriggerZone, Dash_Tweaker } from "dcldash"
+
+
+//System Creation
+class CheckSystem implements ISystem {
+  update(dt: number): void {
+
+    //Write here the logic for cheking if the position of avatar has modified
+}
+}
+//The system needs to be added to engine when ready, uncomment the line below
+//engine.addSystem(new CheckSystem())
+
